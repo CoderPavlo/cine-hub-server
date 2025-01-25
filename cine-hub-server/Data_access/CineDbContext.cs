7a3fd19ec3cc2a37823c46698694d14f087b4b1a
Murcielago4 <kitorianlife@gmail.com>
2025-01-25 21:05:08 +0200

    Create DbContext file and setup settings for conneting in appsettings.json

---
+++ cine-hub-server/Data_access/CineDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace cine_hub_server.Data_access
+{
+    public class CineDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public CineDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+        
+    }
+}
