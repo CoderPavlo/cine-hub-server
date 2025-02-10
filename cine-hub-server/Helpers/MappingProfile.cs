83ac297f0e80cc8a0a4bba56237c3e2b7eebbe81
Murcielago4 <kitorianlife@gmail.com>
2025-02-10 18:28:19 +0200

    Create FilmService, FilmsController, ExeptionMiddleware, HttpException and settings MappingProfile

---
+++ cine-hub-server/Helpers/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using cine_hub_server.DTOs.Film;
+using cine_hub_server.Models;
+
+namespace cine_hub_server.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Film, FilmResponseDto>()
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src =>
+                    src.FilmGenres.Select(fg => fg.Genre.Name)));
+            CreateMap<CreateFilmDto, Film>()
+                .ForMember(dest => dest.FilmGenres, opt => opt.Ignore());
+            CreateMap<UpdateFilmDto, Film>()
+                .ForMember(dest => dest.FilmGenres, opt => opt.Ignore());
+        }
+    }
+}
