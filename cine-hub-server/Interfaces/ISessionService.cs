8bab4081db12d2197fc3dc9941af8998808edb97
ShevSerhii <sergik639@gmail.com>
2025-02-11 11:51:14 +0200

    Add SessionsController with CRUD operations

---
+++ cine-hub-server/Interfaces/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using cine_hub_server.DTOs.Session;
+
+namespace cine_hub_server.Interfaces
+{
+    public interface ISessionService
+    {
+        IEnumerable<SessionResponseDto> GetAll();
+        SessionResponseDto GetById(string id);
+        void Create(CreateSessionDto sessionDto);
+        void Update(string id, UpdateSessionDto sessionDto);
+        void Delete(string id);
+    }
+}
